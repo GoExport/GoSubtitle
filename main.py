a9874f28e1d9eaa4c21a08d117b9d45918772e34
Chloe <dev@lexian.dev>
2025-10-13 13:35:13 -0400

    Refactor main function to include console check and parameter handling

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import sys
 from PyQt6.QtWidgets import QApplication
 from modules import MainWindow
+from modules import Console
+from modules import Parameters
+import helpers
 
 def main():
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec()
+    params = Parameters()
+
+    # Check if the console is attached
+    if not helpers.has_console() or params.get_param_value('gui'):
+        app = QApplication([])
+        window = MainWindow()
+        window.show()
+        app.exec()
+        sys.exit()
+    else:
+        app = Console()
+        app.run(params=params)
 
 if __name__ == "__main__":
     main()
